643c356b5119872891c2f84d8ef568ea6b4b30a6
zhanggui <gui.zhang@100credit.com>
2017-07-11 18:21:16 +0800

    sticker pack extension

---
+++ MessageExtensionDemo/MessageExtensionDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MessageExtensionDemo
+//
+//  Created by zhanggui on 2017/7/6.
+//  Copyright © 2017年 zhanggui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
